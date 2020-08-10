1b68a3836cb1c9501f7432d618c0a776c2fe271c
shalu401 <shalu@dewhive.com>
2020-08-10 11:30:40 +0530

    home screen

--- src/ui-pages/UserHome/components/Content/HomeScreen/index.js
+++ src/ui-pages/UserHome/components/Content/HomeScreen/index.js
@@ -1,42 +1,101 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
-import RestoreIcon from '@material-ui/icons/Restore';
-import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-import HomeIcon from '@material-ui/icons/Home';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import TodayIcon from '@material-ui/icons/Today';
-import ReceiptIcon from '@material-ui/icons/Receipt';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles,Grid,Button,Paper } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
 import { useHistory } from "react-router";
-const useStyles = makeStyles({
-  root: {
-    width: 360,
-    position:"fixed",
-    top: "auto", background: "#eeeeee", bottom: 0 
-  },
-});
 
-export default function LabelBottomNavigation(props) {
+function TabContainer(props) {
   const history = useHistory();
-  const classes = useStyles();
-  const [value, setValue] = React.useState('recents');
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
+  console.log(props.childern, "iiii")
   return (
-    <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
-      <BottomNavigationAction label="Home" value="recents" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Chat" value="favorites" icon={<ChatOutlinedIcon />} 
-      onClick={()=>history.push("/user-home/user-screen/chat")}/>
-      <BottomNavigationAction label="Calendar" value="nearby" icon={<DashboardIcon />} 
-      onClick={()=>history.push("/user-home/user-screen/calendar")}/>
-      <BottomNavigationAction label="Profile" value="folder" icon={<PersonOutlinedIcon />} 
-      onClick={()=>history.push("/user-home/user-screen/specialists")}/>
-      <BottomNavigationAction label="Records" value="fol" icon={<ReceiptIcon />} />
-    </BottomNavigation>
+    <Typography component="div">
+      {/* {props.children==="one"?history.push("/user-home/user-screen/specialists"):history.push("/user-home/user-screen/symptoms")} */}
+    </Typography>
   );
 }
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+class TabsWrappedLabel extends React.Component {
+  state = {
+    value: "one"
+  };
+  componentDidMount = () => {
+    const { history } = this.props
+    history.push("/user-home/home/symptoms")
+  }
+  handleChange = (event, value) => {
+    const { history } = this.props
+    this.setState({ value });
+    if (value === "one") {
+      history.push("/user-home/home/symptoms")
+    }
+    else {
+      history.push("/user-home/home/specialists")
+    }
+  };
+  handleNextButton=()=>{
+    debugger
+    const{history}=this.props;
+    history.push("/user-home/book-appointment")
+  }
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+    const{handleNextButton}=this
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" >
+          <Tabs variant="fullWidth" position="fixed" value={value} onChange={this.handleChange}>
+            <Tab
+              value="one"
+              label="SYMPTOMS" style={{ fontSize: "14px" }}
+            />
+            <Tab value="two" label="SPECIALISTS" style={{ fontSize: "14px" }} />
+          </Tabs>
+        </AppBar>
+        {value === "one" && <TabContainer>Item One</TabContainer>}
+        {value === "two" && <TabContainer>Item Two</TabContainer>}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Paper typeof="button"
+            style={{
+              width:"70px",
+              height:"30px",
+              position: "fixed",
+              bottom: "62px",
+              // padding: "8px",
+              borderRadius: "40px",
+              background:"#2FC9B9"
+            }} onClick={()=>handleNextButton()}
+          ><Typography style={{display: "flex",marginTop:"7%",
+            justifyContent: "center",
+            color: "aliceblue"}}
+        >NEXT</Typography>
+                  <Button
+                ></Button>
+        </Paper>
+        </div>
+      </div>
+    );
+  }
+}
+
+TabsWrappedLabel.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(TabsWrappedLabel);
