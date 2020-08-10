efc9dc30665ddaa9377ae2e358677329ab0a831b
shalu401 <shalu@dewhive.com>
2020-08-10 19:09:08 +0530

    checked added in symptoms and specialities

--- src/ui-pages/UserHome/components/Content/Specialists/index.js
+++ src/ui-pages/UserHome/components/Content/Specialists/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { Card, Grid, CardContent, Typography, Avatar } from "@material-ui/core";
+import { connect } from "react-redux";
+import { httpRequest } from "../../../../../ui-utils/api";
+import { mapDispatchToProps } from "../../../../../ui-utils/commons";
 
 class Specialists extends React.Component {
+  componentDidMount=()=>{
+    const {setAppData,specialists}=this.props
+    setAppData("specialists",{...specialists,generalPhysician:false,pediatrician:false,phychiatrist:false,
+      gynaecologist:false,dermatologist:false
+     })
+  }
+  checkedHandle=(key,value)=>{
+    const {setAppData}=this.props
+    setAppData(key,value)
+  }
   render() {
-    const { history } = this.props
+    const { history,specialists } = this.props
+    const {checkedHandle}=this
     return (
       <div style={{ background: "#eeeeee", height: "100vh" }}>
         <Grid
@@ -15,8 +29,11 @@ class Specialists extends React.Component {
           <Typography variant="h6" color="textSecondary">Find your doctor by specialists</Typography>
           {"\n"}
         </Grid>
-        <Card style={{ margin: "0px 15px 15px 15px" }}>
+        <Card style={{ margin: "0px 15px 15px 15px" }} onClick={()=>checkedHandle("specialists.generalPhysician",!specialists.generalPhysician)}>
           <CardContent>
+          {specialists.generalPhysician===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
             <Grid style={{ display: "flex" }}>
               <Grid item xs={3}>
               <img width="110%" height="110%" src='ic_physician_f.svg' alt="verify_icon" />
@@ -30,8 +47,11 @@ class Specialists extends React.Component {
             </Grid>
           </CardContent>
         </Card>
-        <Card style={{ margin: "15px" }}>
+        <Card style={{ margin: "15px" }} onClick={()=>checkedHandle("specialists.pediatrician",!specialists.pediatrician)}>
           <CardContent>
+          {specialists.pediatrician===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
             <Grid style={{ display: "flex" }}>
               <Grid item xs={3}>
               <img width="110%" height="110%" src='ic_pediatrician_f.svg' alt="verify_icon" />
@@ -45,8 +65,11 @@ class Specialists extends React.Component {
             </Grid>
           </CardContent>
         </Card>
-        <Card style={{ margin: "15px" }}>
+        <Card style={{ margin: "15px" }} onClick={()=>checkedHandle("specialists.phychiatrist",!specialists.phychiatrist)}>
           <CardContent>
+          {specialists.phychiatrist===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
             <Grid style={{ display: "flex" }}>
               <Grid item xs={3}>
               <img width="110%" height="110%" src='ic_psychiatrist_f.svg' alt="verify_icon" />
@@ -60,8 +83,11 @@ class Specialists extends React.Component {
             </Grid>
           </CardContent>
         </Card>
-        <Card style={{ margin: "15px" }}>
+        <Card style={{ margin: "15px" }} onClick={()=>checkedHandle("specialists.gynaecologist",!specialists.gynaecologist)}>
           <CardContent>
+          {specialists.gynaecologist===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
             <Grid style={{ display: "flex" }}>
               <Grid item xs={3}>
               <img width="110%" height="110%" src='ic_gynaecologist_f.svg' alt="verify_icon" />
@@ -75,8 +101,11 @@ class Specialists extends React.Component {
             </Grid>
           </CardContent>
         </Card>
-        <Card style={{ margin: "15px" }}>
+        <Card style={{ margin: "15px" }} onClick={()=>checkedHandle("specialists.dermatologist",!specialists.dermatologist)}>
           <CardContent>
+          {specialists.dermatologist===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
             <Grid style={{ display: "flex" }}>
               <Grid item xs={3}>
               <img width="110%" height="110%" src='ic_dermatologist_f.svg' alt="verify_icon" />
@@ -94,4 +123,13 @@ class Specialists extends React.Component {
     );
   }
 }
-export default Specialists;
+const mapStateToProps = ({ screenConfiguration }) => {
+  const { preparedFinalObject = {} } = screenConfiguration;
+  const { specialists = {} } = preparedFinalObject;
+  return { specialists}
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Specialists)
--- src/ui-pages/UserHome/components/Content/Symptoms/index.js
+++ src/ui-pages/UserHome/components/Content/Symptoms/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Card, Grid, CardContent, Typography, Avatar, TextField } from "@material-ui/core";
-
+import { connect } from "react-redux";
+import { httpRequest } from "../../../../../ui-utils/api";
+import { mapDispatchToProps } from "../../../../../ui-utils/commons";
 class Symptoms extends React.Component {
+  componentDidMount=()=>{
+    const {setAppData,symptoms}=this.props
+    setAppData("symptoms",{...symptoms,abdomen:false,back:false,chest:false,ear:false,head:false,piles:false,
+      tooth:false,chills:false,fever:false,lightHeaded:false,feve:false})
+  }
+  checkedHandle=(key,value)=>{
+    const {symptoms,setAppData}=this.props
+    setAppData(key,value)
+  }
   render() {
-    const { history } = this.props
+    const { history,symptoms } = this.props
+    const {checkedHandle}=this
     return (
       <div style={{ background: "#eeeeee", height: "100vh" }}>
         <Grid
@@ -28,32 +40,57 @@ class Symptoms extends React.Component {
             <TextField fullWidth InputProps={{ readOnly: true, style: { fontSize: 0 } }} />
           </Grid>
           <Grid container align="center"  >
-            <Grid item xs={3} style={{ padding: "10px" }}>
-              <img height ="20px" width="20px" src="order-placed.png" style={{ position: "absolute" }}></img>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.abdomen",!symptoms.abdomen)}>
+              {symptoms.abdomen===true?
+              <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",
+              zIndex:"1",background:"#F7F7F7",borderRadius: "50%"}}></img>:""}
               <Avatar style={{ height: "45px", width: "45px",position: "relative"  }} src="ic_eight.svg"></Avatar>
               <Typography>ABDOMEN</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.back",!symptoms.back)}>
+              {symptoms.back===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
+
               <Avatar style={{ height: "45px", width: "45px" }} src="ic_eight.svg" ></Avatar>
               <Typography>BACK</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.chest",!symptoms.chest)}>
+              {symptoms.chest===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
+
               <Avatar style={{ height: "45px", width: "45px" }}  src="ic_three.svg" ></Avatar>
               <Typography>CHEST</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.ear",!symptoms.ear)}>
+              {symptoms.ear===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
+
               <Avatar style={{ height: "45px", width: "45px" }} src="ic_add_square_o.svg"></Avatar>
               <Typography>EAR</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.head",!symptoms.head)}>
+              {symptoms.head===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
+
               <Avatar style={{ height: "45px", width: "45px" }} src="ic_add_square_o.svg"></Avatar>
               <Typography>HEAD</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.piles",!symptoms.piles)}>
+              {symptoms.piles===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
+
               <Avatar style={{ height: "45px", width: "45px" }} src="ic_eleven.svg"></Avatar>
               <Typography>PILES</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.tooth",!symptoms.tooth)}>
+              {symptoms.tooth===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
               <Avatar style={{ height: "45px", width: "45px" }} src="ic_add_square_o.svg"></Avatar>
               <Typography>TOOTH</Typography>
             </Grid>
@@ -70,19 +107,31 @@ class Symptoms extends React.Component {
             <TextField fullWidth InputProps={{ readOnly: true, style: { fontSize: 0 } }} />
           </Grid>
           <Grid container align="center"  >
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.chills",!symptoms.chills)}>
+              {symptoms.chills===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
               <Avatar src="ic_add_square_o.svg" style={{ height: "45px", width: "45px" }} ></Avatar>
               <Typography>CHILLS</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.fever",!symptoms.fever)}>
+              {symptoms.fever===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
               <Avatar src="ic_add_square_o.svg" style={{ height: "45px", width: "45px" }} ></Avatar>
               <Typography>FEVER</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.lightHeaded",!symptoms.lightHeaded)}>
+              {symptoms.lightHeaded===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
               <Avatar src="ic_add_square_o.svg" style={{ height: "45px", width: "45px" }} ></Avatar>
               <Typography>LIGHT-HEADED</Typography>
             </Grid>
-            <Grid item xs={3} style={{ padding: "10px" }}>
+            <Grid item xs={3} style={{ padding: "10px" }} onClick={()=>checkedHandle("symptoms.feve",!symptoms.feve)}>
+              {symptoms.feve===true?
+            <img height ="20px" width="20px" src="check_circle.svg" style={{ position: "absolute",zIndex:"1",
+            background:"white",borderRadius: "50%"}}></img>:""}
               <Avatar src="ic_add_square_o.svg" style={{ height: "45px", width: "45px" }} ></Avatar>
               <Typography>FEVER</Typography>
             </Grid>
@@ -92,4 +141,13 @@ class Symptoms extends React.Component {
     );
   }
 }
-export default Symptoms
+const mapStateToProps = ({ screenConfiguration }) => {
+  const { preparedFinalObject = {} } = screenConfiguration;
+  const { symptoms = {} } = preparedFinalObject;
+  return { symptoms}
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Symptoms)
