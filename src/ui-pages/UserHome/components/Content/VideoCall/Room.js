4772d85e9bd2ad74008befe6d853b87dd7107974
heyneha10 <neha@dewhive.com>
2020-08-26 16:26:13 +0530

    video call component integrated

---
+++ src/ui-pages/UserHome/components/Content/VideoCall/Room.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import Video from 'twilio-video';
+import Participant from './Participant';
+
+const Room = ({ roomName, token, handleLogout, muteAudio }) => {
+  const [room, setRoom] = useState(null);
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+    const participantConnected = participant => {
+      setParticipants(prevParticipants => [...prevParticipants, participant]);
+    };
+
+    const participantDisconnected = participant => {
+      setParticipants(prevParticipants =>
+        prevParticipants.filter(p => p !== participant)
+      );
+    };
+
+    Video.connect(token, {
+      name: roomName,
+      audio:true,
+      video:{width:100,height:100}
+    }).then(room => {
+      setRoom(room);
+      room.on('participantConnected', participantConnected);
+      room.on('participantDisconnected', participantDisconnected);
+      room.participants.forEach(participantConnected);
+    });
+
+    return () => {
+      setRoom(currentRoom => {
+        if (currentRoom && currentRoom.localParticipant.state === 'connected') {
+          currentRoom.localParticipant.tracks.forEach(function(trackPublication) {
+            trackPublication.track.stop();
+          });
+          currentRoom.disconnect();
+          return null;
+        } else {
+          return currentRoom;
+        }
+      });
+    };
+  }, [roomName, token]);
+
+  const remoteParticipants = participants.map(participant => (
+    <Participant key={participant.sid} participant={participant} />
+  ));
+
+  return (<div>
+    <div className="room">
+      <h2>Room: {roomName}</h2>
+    
+      <div className="local-participant">
+        {room ? (
+          <Participant
+            key={room.localParticipant.sid}
+            participant={room.localParticipant}
+          />
+        ) : (
+          ''
+        )}
+      </div>
+      <h3>Remote Participants</h3>
+      <div className="remote-participants">{remoteParticipants}</div>
+    </div>
+    <button onClick={handleLogout}>Log out</button>
+    <button onClick={muteAudio}>Mute</button>
+    </div>
+  );
+};
+
+export default Room;
---
+++ src/ui-pages/UserHome/components/Content/VideoCall/Video.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Room from './Room';
+import Video from 'twilio-video';
+
+const VideoChat = () => {
+const roomName="7888683799-8447594602";
+const token="eyJjdHkiOiJ0d2lsaW8tZnBhO3Y9MSIsInR5cCI6IkpXVCIsImFsZyI6IkhTMjU2In0.eyJpc3MiOiJTS2U1ZmIzM2Q5N2ZkZmY1YTg5ZjUwMDFiMWRlMTE4ODZmIiwiZXhwIjoxNTk4NDM5MjkwLCJncmFudHMiOnsiaWRlbnRpdHkiOiJQYXRpZW50IiwidmlkZW8iOnsicm9vbSI6Ijc4ODg2ODM3OTktODQ0NzU5NDYwMiJ9fSwianRpIjoiU0tlNWZiMzNkOTdmZGZmNWE4OWY1MDAxYjFkZTExODg2Zi0xNTk4NDM1NzEyIiwic3ViIjoiQUNlYWJkYzQxY2U0ZGI3ZmIxOThhYmQ1ZmE0YzhjZDY3MyJ9.DyFXWJeHb3FEuDQEz2I6JWiP6q-o7XFW-LDgFjZ_oZA"
+
+const handleLogout=()=>{
+  Video.connect(token, { name: roomName }).then((room) => {
+
+    room.localParticipant.audioTracks.forEach((publication) => {
+      publication.track.disable();
+    });
+
+    room.localParticipant.videoTracks.forEach((publication) => {
+      publication.track.disable();
+    });
+
+    room.localParticipant.videoTracks.forEach((publication) => {
+      publication.track.stop();
+      publication.unpublish();
+    });
+  });
+}
+
+
+const muteAudio=()=>{
+  Video.connect(token, { name: roomName }).then((room) => {
+    room.localParticipant.audioTracks.forEach((publication) => {
+      publication.track.disable();
+    });
+    room.localParticipant.videoTracks.forEach((publication) => {
+      publication.track.enable();
+    });
+  });
+}
+  let render;
+  return render =  <Room roomName={roomName}
+   token={token}
+   handleLogout={handleLogout} 
+   muteAudio={muteAudio}/>;
+};
+
+export default VideoChat;
