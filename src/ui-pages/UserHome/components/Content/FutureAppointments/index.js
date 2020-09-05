cbe47981eb4c35575fa0aa4cae4d7f00728d9388
shalu401 <shalu@dewhive.com>
2020-09-06 01:11:46 +0530

    Appointment integration

--- src/ui-pages/UserHome/components/Content/FutureAppointments/index.js
+++ src/ui-pages/UserHome/components/Content/FutureAppointments/index.js
@@ -1,43 +1,49 @@
 import React from "react";
-import { Card, Grid, CardContent, Typography, Avatar, Button } from "@material-ui/core";
+import { Card, Grid, CardContent, Typography, Avatar } from "@material-ui/core";
 import { connect } from "react-redux";
 import { mapDispatchToProps } from "../../../../../ui-utils/commons";
 
 class FutureAppointments extends React.Component {
-  handleChange = () => {
-    const { setAppData } = this.props
-    setAppData("")
-  }
   render() {
-    const { history } = this.props
+    const { history, appointments } = this.props
     return (
       <div style={{ background: "#f7f7f7", height: "100vh" }}>
         <div style={{ margin: "0px 15px 15px 15px" }}>
-          <Grid
-            container
-            alignItems="center"
-            justify="center"
-            direction="row"
-            style={{ height: "13vh", marginTop: "3.5%" }}>
-            <Typography align="center" color="textSecondary" style={{ margin: "5px 5px 2px 5px", 
-            fontSize: "15px", fontWeight: 500 }}>Your nest appontment is on May 21st,Thrusday at 9:00 AM with Dr. Michael D. Dombroski</Typography>
-            {"\n"}
-          </Grid>
-          <Card >
-            <CardContent>
-              <Grid style={{ display: "flex" }} onClick={()=>history.push("/user-home/generate-token")}>
-                <Grid item xs={3}>
-                  <Avatar />
+          {appointments.map((data) => {
+            return (
+              <div>
+                <Grid
+                  container
+                  alignItems="center"
+                  justify="center"
+                  direction="row"
+                  style={{ height: "13vh", marginTop: "3.5%" }}>
+                  <Typography align="center" color="textSecondary" style={{
+                    margin: "5px 5px 2px 5px",
+                    fontSize: "15px", fontWeight: 500
+                  }}>Your next appontment is on September
+                {data.date},{data.day}
+                    {""} at {new Date(data.appointment_datetime).getTime()} AM with {data.doctor_name}</Typography>
+                  {"\n"}
                 </Grid>
-                <Grid item md={9}>
-                  <Typography variant="h6">Michael D.Dombroski</Typography>
-                  <Typography color="textSecondary" variant="subtitle2">
-                    General Physician 11 years, MBBS, MD, English and Freanch
-                </Typography>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
+                <Card>
+                  <CardContent>
+                    <Grid style={{ display: "flex" }} onClick={() => history.push("/user-home/generate-token")}>
+                      <Grid item xs={3}>
+                        <Avatar />
+                      </Grid>
+                      <Grid item md={9}>
+                        <Typography variant="h6">{data.doctor_name}</Typography>
+                        <Typography color="textSecondary" variant="subtitle2">
+                          {data.Doctor_speciality}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </div>
+            )
+          })}
         </div>
       </div>
     );
@@ -45,8 +51,10 @@ class FutureAppointments extends React.Component {
 }
 const mapStateToProps = ({ screenConfiguration }) => {
   const { preparedFinalObject = {} } = screenConfiguration;
-  const { confirmBooking = {} } = preparedFinalObject;
-  return { confirmBooking }
+  const { futureAppointments = {} } = preparedFinalObject;
+  const { appointments = [] } = futureAppointments
+
+  return { appointments, futureAppointments }
 };
 
 export default connect(
--- src/ui-pages/UserHome/components/Content/Landing/index.js
+++ src/ui-pages/UserHome/components/Content/Landing/index.js
@@ -1,27 +1,70 @@
 import React from "react";
 import { Card, Grid, CardContent, Typography } from "@material-ui/core";
 import MessageIcon from "@material-ui/icons/Message";
-import { withRouter } from 'react-router-dom';
+import { mapDispatchToProps } from "../../../../../ui-utils/commons";
+import { httpRequest } from "../../../../../ui-utils/api"
+import { connect } from "react-redux";
+
 
 class Landing extends React.Component {
+  componentDidMount = async () => {
+    debugger
+    const { setAppData } = this.props;
+    let tempVar = []
+    let days = [{ number: 1, day: "Mon" }, { number: 2, day: "Tue" }, { number: 3, day: "Wed" }, { number: 4, day: "Thru" }, { number: 5, day: "Fri" },
+    { number: 6, day: "Sat" }, { number: 7, day: "Sun" }]
+    let dat = {}
+    const apiResponse = await httpRequest({
+      endPoint: `/patientAppointmentList/7895328523`,
+      method: "get",
+      instance: "instanceOne",
+    })
+    if (apiResponse) {
+      apiResponse.forEach((data) => {
+        let datee = data.appointment_datetime
+        let da = new Date(datee).getDate()
+        let daa = new Date(datee).getDay()
+        if (data.appointment_datetime === new Date().getDate()) {
+          days.forEach((dataa) => {
+            if (dataa.number === daa) {
+              dat = { ...data, date: da, day: dataa.day, actualDate: data.booking_date }
+              tempVar.push(dat)
+            }
+          })
+          setAppData("landing.todayAppointment", tempVar)
+        }
+        if (data.appointment_datetime !== new Date().getDate()) {
+          days.forEach((dataa) => {
+            if (dataa.number === daa) {
+              dat = { ...data, date: da, day: dataa.day, actualDate: data.booking_date }
+              tempVar.push(dat)
+            }
+          })
+          setAppData("futureAppointments.appointments", tempVar)
+        }
+      })
+    }
+  }
+
   render() {
-    const { history } = this.props
+    const { history, todayAppointment } = this.props
     return (
       <div style={{ background: "#eeeeee", height: "100vh" }}>
-        <Card>
-          <Grid
-            container
-            style={{ background: "#343434", color: "white", padding: "10px", marginTop: "10px" }}>
-            <Typography onClick={()=>history.push("/user-home/generate-token")}>You don't have any upcomming appointment</Typography>
-            <MessageIcon style={{ marginLeft: "4%", marginTop: "1%" }} onClick={()=>history.push("/user-home/video-call")}/>
-          </Grid>
-        </Card>
+        {todayAppointment ?
+          <Card>
+            <Grid
+              container
+              style={{ background: "#343434", color: "white", padding: "10px", marginTop: "10px" }}>
+              <Typography onClick={() => history.push("/user-home/generate-token")}>You don't have any upcomming appointment</Typography>
+              <MessageIcon style={{ marginLeft: "4%", marginTop: "1%" }} onClick={() => history.push("/user-home/video-call")} />
+            </Grid>
+          </Card> : ""}
         <Grid
           container
           alignItems="center"
           justify="center"
           direction="row"
-          style={{ height: "10vh" }}
+          style={{ height: "10vh", marginTop: "40px" }}
         >
           <Typography variant="h6"> Hello Tony S. Ringing,</Typography>
           {"\n"}
@@ -33,7 +76,7 @@ class Landing extends React.Component {
               <Grid item xs={4} >
                 <img width="90%" height="100%" src='ic_video_on.svg' alt="verify_icon" />
               </Grid>
-              <Grid item md={8} onClick={()=>history.push("/user-home/home")}>
+              <Grid item md={8} onClick={() => history.push("/user-home/home")}>
                 <Typography variant="h6"> Video call a Doctor</Typography>
                 <Typography color="textSecondary" variant="subtitle2">
                   You can book a video confrence and meet our doctors sitting at
@@ -47,13 +90,13 @@ class Landing extends React.Component {
           <CardContent>
             <Grid style={{ display: "flex" }}>
               <Grid item xs={4}>
-              <img width="90%" height="100%" src='ic_clinic_o.svg' alt="verify_icon" />
+                <img width="90%" height="100%" src='ic_clinic_o.svg' alt="verify_icon" />
 
               </Grid>
               <Grid item md={8}>
                 <Typography variant="h6"> Clinic Appointment</Typography>
                 <Typography color="textSecondary" variant="subtitle2">
-                You can book a video confrence and meet our doctors sitting at
+                  You can book a video confrence and meet our doctors sitting at
                   comfort in your home
                   </Typography>
               </Grid>
@@ -64,13 +107,13 @@ class Landing extends React.Component {
           <CardContent>
             <Grid style={{ display: "flex" }}>
               <Grid item xs={4}>
-              <img width="90%" height="100%" src='ic_home_clinic_f.svg' alt="verify_icon" />
+                <img width="90%" height="100%" src='ic_home_clinic_f.svg' alt="verify_icon" />
 
               </Grid>
               <Grid item md={8}>
                 <Typography variant="h6"> Doctor at Home</Typography>
                 <Typography color="textSecondary" variant="subtitle2">
-                You can book a video confrence and meet our doctors sitting at comfort in your home
+                  You can book a video confrence and meet our doctors sitting at comfort in your home
                  </Typography>
               </Grid>
             </Grid>
@@ -80,4 +123,16 @@ class Landing extends React.Component {
     );
   }
 }
-export default withRouter(Landing) ;
+// export default withRouter(Landing) ;
+const mapStateToProps = ({ screenConfiguration }) => {
+  const { preparedFinalObject = {} } = screenConfiguration;
+  const { login = {}, landing = {} } = preparedFinalObject;
+  const { todayAppointment } = landing
+  const { phoneno } = login
+  return { phoneno, landing, todayAppointment }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Landing)
